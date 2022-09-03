0d879dcd3a96e57eb43949cd3c5da6516caecbc2
compensations <112657862+compensations@users.noreply.github.com>
2022-09-03 13:49:26 -0400

    Update bypassroblox.bat

--- bypassroblox.bat
+++ bypassroblox.bat
@@ -1,6 +1,5 @@
 @echo off
 echo **This file must be ran with administrator privileges**
-echo Thanks to BrentDaMage for letting me know about this.
 echo 128.116.119.4 www.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
 echo 128.116.119.4 web.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
 echo 128.116.119.4 abtesting.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
